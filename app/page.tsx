92b3523ab5ad45db8a369bc3cf18ee4c9ecc4fb8
shicks1080 <stuart.hicks@gmail.com>
2023-08-01 14:39:29 +0200

    housekeeping: Move the Timeline to it's own component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 import MenuSidebar from "@/components/menu/MenuSidebar"
-import Avatar from "@/components/common/avatar"
-import BeATwittButton from "@/components/common/BeATwittButton"
-import TwittCard from "@/components/twittcard/TwittCard"
+import Timeline from "@/components/timeline/Timeline"
 
 const Home = () => {
   return (
@@ -9,33 +7,7 @@ const Home = () => {
       <div className='max-w-screen-xl w-full h-full flex relative'>
         {/* left sidebar for navigation */}
         <MenuSidebar />
-        <main className='ml-72 flex w-full max-w-[600px] h-full min-h-screen flex-col border-l-[0.5px] border-r-[0.5px] border-primaryDark'>
-          <h1 className='text-xl p-6 font-bold backdrop-blur bg-primaryDark/10 sticky top-0'>Home</h1>
-          <div className='border-t-[0.5px] border-b-[0.5px] flex items-start p-4 space-x-2 border-primaryDark relative'>
-            <div className='flex-none'>
-              <Avatar />
-            </div>
-            <div className='flex flex-col w-full h-full'>
-              <input
-                type='text'
-                placeholder='Whassup?'
-                className='w-full h-full p-4 bg-transparent outline-none border-none placeholder:text-2xl placeholder:text-primaryDark/50'
-              />
-              <div className='w-full justify-between items-center flex'>
-                <div>Icons</div>
-                {/* Need to style the component, can follow a tutorial:
-                  https://www.smashingmagazine.com/2020/05/reusable-react-components-tailwind/
-                 */}
-                <BeATwittButton />
-              </div>
-            </div>
-          </div>
-          <div className='flex flex-col'>
-            {Array.from({ length: 5 }).map((_, i) => (
-              <TwittCard key={i} index={i}/>
-            ))}
-          </div>
-        </main>
+        <Timeline />
         <section>right-sidebar</section>
       </div>
     </div>
