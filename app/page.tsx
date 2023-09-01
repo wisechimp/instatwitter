8db5cffcc7864f7f9f82a58b831c968ad1b0c617
shicks1080 <stuart.hicks@gmail.com>
2023-09-01 14:34:38 +0200

    feat: Adding the right hand side to the app
    Extracting the Menu items to a data file and adding a reference to that in the app.

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
-import MenuSidebar from "@/components/menu/MenuSidebar"
-import Timeline from "@/components/timeline/Timeline"
+import navigationMenuData from "@/data/navigation"
+import MenuSidebar from "./components/menu/MenuSidebar"
+import Timeline from "./components/timeline/Timeline"
+import WhatsUpSidebar from "./components/explorationsidebar/ExplorationSidebar"
 
 const Home = () => {
   return (
     <div className='w-full h-full flex justify-center items-center relative bg-primaryLight text-primaryDark'>
       <div className='max-w-screen-xl w-full h-full flex relative'>
         {/* left sidebar for navigation */}
-        <MenuSidebar />
+        <MenuSidebar data={navigationMenuData} />
         <Timeline />
-        <section>right-sidebar</section>
+        <WhatsUpSidebar />
       </div>
     </div>
   )
