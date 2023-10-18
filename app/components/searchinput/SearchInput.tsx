17b500f411bf698be2cb7978fad2c40402fde269
shicks1080 <stuart.hicks@gmail.com>
2023-10-18 15:09:22 +0200

    styling: Finishing the Search button

--- app/components/searchinput/SearchInput.tsx
+++ app/components/searchinput/SearchInput.tsx
@@ -8,18 +8,18 @@ const SearchInput = ({ placeholderText }: SearchInputProps) => {
   return (
     <div className='m-4 '>
       <div className='relative w-full h-full'>
-        <label
-          htmlFor='searchBox'
-          className='absolute top-0 left-4 h-full flex items-center justify-center'
-        >
-          <BsSearch className='w-5 h-5 text-primaryDark' />
-        </label>
         <input
           id='searchBox'
           type='text'
           placeholder={placeholderText}
           className='rounded-3xl py-4 px-12 outline-none border-[1px] border-primaryDark focus:border focus:border-white bg-primaryDark/10 placeholder:text-primaryDark/70'
         />
+        <label
+          htmlFor='searchBox'
+          className='absolute top-0 left-4 h-full flex items-center justify-center text-primaryDark'
+        >
+          <BsSearch className='w-5 h-5' />
+        </label>
       </div>
     </div>
   )
