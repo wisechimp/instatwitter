ee526892f5acd5dd9f930994d03f879de3267701
Stuart Hicks <stuart.hicks@gmail.com>
2023-10-24 19:57:54 +0200

    styling: Adding styling to login page
    Adding state for conditional styling of buttons and appearance of the username inout.
    More work required but committing as switching machines!

--- app/((site))/login/page.tsx
+++ app/((site))/login/page.tsx
@@ -2,38 +2,63 @@
 import { useState } from 'react'
 
 const Login = () => {
+  const [signupOption, setSignupOption] = useState(false)
   const [username, setUsername] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
+  const handleSignupOption = (status: boolean) => {
+    console.log(status)
+    return setSignupOption(!status)
+  }
+
 
   return (
-    <div>
-      <h1>Instatwitt Login</h1>
-      <form action="/auth/signin" method="post">
-        <label htmlFor='username'>Username</label>
-        <input 
-          name='username'
-          onChange={(e) => setUsername(e.target.value)}
-          value={username}
+    <div className="bg-primaryLight h-screen">
+      <h1 className='text-xl font-bold text-primaryDark p-4'>Instatwitt Login</h1>
+      <div className='flex flex-col justify-center'>
+      <div className="flex flex-row justify-around w-1/4 m-auto p4">
+        <button className={`w-full py-4 ${signupOption ? "bg-primaryLight": "bg-primaryCentral"}`} disabled={signupOption} onClick={() => handleSignupOption(signupOption)}>Sign Up</button>
+        <button className={`w-full py-4 ${signupOption ? "bg-primaryCentral": "bg-primaryLight"}`} disabled={!signupOption} onClick={() => handleSignupOption(signupOption)}>Sign In</button>
+      </div>
+        <form action="/auth/signin" method="post">
+      <div className="flex flex-col justify-center w-1/4 m-auto p-4">
+        <div className={`flex justify-between py-4 ${signupOption ? "" : "hidden" }`}>
+          <label htmlFor='username'>Username:</label>
+          <input 
+          className='border-2 border-primaryDark'
+            name='username'
+            onChange={(e) => setUsername(e.target.value)}
+            value={username}
+            />
+            </div>
+            <div  className='flex justify-between py-4'>
+          <label htmlFor='email'>Email:</label>
+          <input
+          className='border-2 border-primaryDark'
+            name='email'
+            type='email'
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
           />
-        <label htmlFor='email'>Email</label>
-        <input
-          name='email'
-          type='email'
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
-        />
-        <label htmlFor='password'>Password</label>
-        <input
-          type='password'
-          name='password'
-          onChange={(e) => setPassword(e.target.value)}
-          value={password}
-        />
-        <button formAction="/auth/signup">Sign up</button>
-        <button>Sign in</button>
-      </form>
+          </div>
+          <div className='flex justify-between py-4'>
+          <label htmlFor='password'>Password:</label>
+          <input
+          className='border-2 border-primaryDark'
+            type='password'
+            name='password'
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
+          </div>
+          <div className='flex justify-center'>
+          <button className={`bg-primaryCentral px-4 py-2 m-2 rounded-lg ${signupOption ? "" : "hidden" }`} formAction="/auth/signup">Sign up</button>
+          <button className={`bg-primaryCentral px-4 py-2 m-2 rounded-lg ${signupOption ? "hidden" : "" }`}>Sign in</button>
+          </div>
+          </div>
+        </form>
+        </div>
     </div>
   )
 }
